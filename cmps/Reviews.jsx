ad1fee78516c0916ac76b779a0afdf122d101c81
yifatre <yifat.reg@gmail.com>
2024-03-12 10:34:36 +0200

    Update colors

--- cmps/Reviews.jsx
+++ cmps/Reviews.jsx
@@ -1,4 +1,12 @@
 export function Reviews({ reviews }) {
-    return <pre>{JSON.stringify(reviews, null, 2)}</pre>
+    return <ul className="reviews flex flex-column clean-list">
+        {reviews.map(review =>
+            <li key={review.id} className="grid">
+                <span>Name: {review.fullname}</span>
+                <span>{'⭐'.repeat(review.rating)}</span>
+                <span>Read on: {review.readOn}</span>
+            </li>)}
+    </ul>
+    // <pre>{JSON.stringify(reviews, null, 2)}</pre>
 
 }
